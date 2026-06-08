45e196cb1c045f0845bd5a7cfc0aec3ed1a32bb9
Gwh22112 <fwphowuo@gmail.com>
2026-06-08 12:19:36 +0200

    Update antiafk.lua

--- antiafk.lua
+++ antiafk.lua
@@ -1,1 +1,16 @@
+return function()
+    return function(State)
 
+        local Players = game:GetService("Players")
+        local VirtualUser = game:GetService("VirtualUser")
+
+        Players.LocalPlayer.Idled:Connect(function()
+            if State.AntiAFK then
+                VirtualUser:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+                task.wait(1)
+                VirtualUser:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+            end
+        end)
+
+    end
+end
