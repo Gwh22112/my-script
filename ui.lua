387298f45d957930f6119d7219a2aafdad494280
Gwh22112 <fwphowuo@gmail.com>
2026-06-08 12:20:24 +0200

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -1,1 +1,25 @@
+return function()
+    return function(State)
 
+        print("UI Loaded")
+
+        -- 🔥 SIMPLE TEST CONTROLS (you can remove later)
+
+        task.spawn(function()
+            while true do
+                task.wait(5)
+
+                print("AntiAFK:", State.AntiAFK)
+                print("Filter Enabled:", State.IngredientFilter.Enabled)
+                print("Filter Name:", State.IngredientFilter.Name)
+                print("Filter Rarity:", State.IngredientFilter.Rarity)
+            end
+        end)
+
+        -- Example toggles (you can hook to real UI later)
+        State.IngredientFilter.Enabled = true
+        State.IngredientFilter.Name = ""
+        State.IngredientFilter.Rarity = "Any"
+
+    end
+end
