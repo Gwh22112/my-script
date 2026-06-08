06af4ab2f834a37c6bc3f9ed407bb86d3e79f451
Gwh22112 <fwphowuo@gmail.com>
2026-06-08 12:19:19 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,26 @@
+return function()
 
+    local base = "https://raw.githubusercontent.com/Gwh22112/my-script/main/"
+
+    local State = {
+        AntiAFK = true,
+
+        IngredientFilter = {
+            Enabled = false,
+            Name = "",
+            Rarity = "Any"
+        }
+    }
+
+    local UI = loadstring(game:HttpGet(base .. "ui.lua"))()()
+    local Systems = loadstring(game:HttpGet(base .. "systems.lua"))()()
+    local Features = loadstring(game:HttpGet(base .. "features.lua"))()()
+    local Filter = loadstring(game:HttpGet(base .. "filter.lua"))()()
+    local AntiAFK = loadstring(game:HttpGet(base .. "antiafk.lua"))()()
+
+    Systems(State)
+    AntiAFK(State)
+    Features(State, Filter)
+    UI(State)
+
+end
