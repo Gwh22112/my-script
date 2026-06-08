214400965e7b5be6cf72f0dd160389e9095a791a
Gwh22112 <fwphowuo@gmail.com>
2026-06-08 12:20:11 +0200

    Update systems.lua

--- systems.lua
+++ systems.lua
@@ -1,1 +1,11 @@
+return function()
+    return function(State)
 
+        local RunService = game:GetService("RunService")
+
+        RunService.RenderStepped:Connect(function()
+            -- you can add stuff here later if needed
+        end)
+
+    end
+end
