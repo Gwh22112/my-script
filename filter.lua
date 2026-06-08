b8b87f5068cc5f1c9eb145c5b316a2a047b5d22d
Gwh22112 <fwphowuo@gmail.com>
2026-06-08 12:26:09 +0200

    Update filter.lua

--- filter.lua
+++ filter.lua
@@ -1,23 +1,28 @@
 return function()
     return function(State)
 
-        return function(ingredient)
+        return function(itemName)
 
             if not State.IngredientFilter.Enabled then
                 return true
             end
 
+            local data = Ingredients[itemName]
+            if not data then return false end
+
+            -- NAME FILTER
             if State.IngredientFilter.Name ~= "" then
                 if not string.find(
-                    string.lower(ingredient.Name),
+                    string.lower(itemName),
                     string.lower(State.IngredientFilter.Name)
                 ) then
                     return false
                 end
             end
 
+            -- RARITY FILTER
             if State.IngredientFilter.Rarity ~= "Any" then
-                if ingredient.Rarity ~= State.IngredientFilter.Rarity then
+                if data.Rarity ~= State.IngredientFilter.Rarity then
                     return false
                 end
             end
