d77e0c7eb6d92969f6bda7afa8ebe600ebb121e8
Gwh22112 <fwphowuo@gmail.com>
2026-06-08 12:19:48 +0200

    Update filter.lua

--- filter.lua
+++ filter.lua
@@ -1,1 +1,29 @@
+return function()
+    return function(State)
 
+        return function(ingredient)
+
+            if not State.IngredientFilter.Enabled then
+                return true
+            end
+
+            if State.IngredientFilter.Name ~= "" then
+                if not string.find(
+                    string.lower(ingredient.Name),
+                    string.lower(State.IngredientFilter.Name)
+                ) then
+                    return false
+                end
+            end
+
+            if State.IngredientFilter.Rarity ~= "Any" then
+                if ingredient.Rarity ~= State.IngredientFilter.Rarity then
+                    return false
+                end
+            end
+
+            return true
+        end
+
+    end
+end
